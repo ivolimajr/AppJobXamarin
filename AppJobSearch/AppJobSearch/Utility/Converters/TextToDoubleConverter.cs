cf185c193492c19fabcad7b31ba953eb9e657072
Ivo Lima Jr <ivo.sl.jr@gmail.com>
2021-12-04 19:51:40 -0300

    git ignore , add job

---
+++ AppJobSearch/AppJobSearch/Utility/Converters/TextToDoubleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace AppJobSearch.Utility.Converters
+{
+    public class TextToDoubleConverter
+    {
+        public static Double ToDouble(string value)
+        {
+            if (value != null)
+            {
+                value = RemoveExtraText(value);
+                return Double.Parse(value);
+            }
+            return 0;
+        }
+
+        private static string RemoveExtraText(string value)
+        {
+            var allowedChars = "01234567890.,";
+            return new string(value.Where(c => allowedChars.Contains(c)).ToArray());
+        }
+    }
+}
--- AppJobSearch/AppJobSearch/Views/RegisterJob.xaml.cs
+++ AppJobSearch/AppJobSearch/Views/RegisterJob.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System;
-
+﻿using AppJobSearch.Models;
+using AppJobSearch.Services;
+using AppJobSearch.Utility.Converters;
+using AppJobSearch.Utility.Load;
+using Newtonsoft.Json;
+using Rg.Plugins.Popup.Extensions;
+using System;
+using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,9 +14,11 @@ namespace AppJobSearch.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RegisterJob : ContentPage
     {
+        private JobService _service;
         public RegisterJob()
         {
             InitializeComponent();
+            _service = new JobService();
         }
 
         private void btnStartPage(object sender, EventArgs e)
@@ -18,10 +26,61 @@ namespace AppJobSearch.Views
             Navigation.PopAsync();
         }
 
-        private void btnSaveJob(object sender, EventArgs e)
+        private async void btnSaveJob(object sender, EventArgs e)
         {
-            DisplayAlert("Salvo", "Registrado!", "Ok");
-            Navigation.PopAsync();
+            //TODO - Adicionar Validação
+            TxtMessages.Text = String.Empty;
+
+            User user = JsonConvert.DeserializeObject<User>(App.Current.Properties["User"].ToString());
+
+            Job job = new Job()
+            {
+                Company = TxtCompany.Text,
+                JobTitle = TxtJobTitle.Text,
+                CityState = TxtCityState.Text,
+                InitialSalary = TextToDoubleConverter.ToDouble(TxtInitialSalary.Text),
+                FinalSalary = TextToDoubleConverter.ToDouble(TxtFinalSalary.Text),
+                ContractType = RBCLT.IsChecked ? "CLT" : "PJ",
+                TecnologyTools = TxtTecnologyTools.Text,
+                CompanyDescription = TxtCompanyDescription.Text,
+                JobDescription = TxtJobDescription.Text,
+                Benefits = TxtBenefits.Text,
+                InterestedSendEmailTo = TxtInterestedSendEmailTo.Text,
+                PublicationDate = DateTime.Now,
+                UserId = user.Id
+            };
+
+            await Navigation.PushPopupAsync(new Loading());
+            ResponseService<Job> responseService = await _service.AddJob(job);
+
+            if (responseService.IsSuccess)
+            {
+                await Navigation.PopAllPopupAsync();
+                await DisplayAlert("Vaga cadastrada!", "Vaga cadastrada com sucesso!", "OK");
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                if (responseService.StatusCode == 400)
+                {
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (var dicKey in responseService.Errors)
+                    {
+                        foreach (var message in dicKey.Value)
+                        {
+                            sb.AppendLine(message);
+                        }
+                    }
+
+                    TxtMessages.Text = sb.ToString();
+                }
+                else
+                {
+                    await DisplayAlert("Erro!", "Oops! Ocorreu um erro inesperado! Tente novamente mais tarde.", "OK");
+                }
+                await Navigation.PopAllPopupAsync();
+            }
         }
     }
 }
