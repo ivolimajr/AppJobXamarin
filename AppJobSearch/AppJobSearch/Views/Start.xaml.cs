bec4b0e41fab11d681dc7a5936ff40eaccf7bf34
Ivo Lima Jr <ivo.sl.jr@gmail.com>
2021-12-04 22:53:34 -0300

    final version

--- AppJobSearch/AppJobSearch/Views/Start.xaml.cs
+++ AppJobSearch/AppJobSearch/Views/Start.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Xamarin.Forms;
+﻿using AppJobSearch.Models;
+using AppJobSearch.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AppJobSearch.Views
@@ -6,9 +12,15 @@ namespace AppJobSearch.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Start : ContentPage
     {
+        private JobService _service;
+        private ObservableCollection<Job> _listOfJobs;
+        private SearchParams _searchParams;
+        private int _listOfJobsFirstRequest;
+
         public Start()
         {
             InitializeComponent();
+            _service = new JobService();
         }
 
         private void btnAddJob(object sender, System.EventArgs e)
@@ -16,24 +28,63 @@ namespace AppJobSearch.Views
             Navigation.PushAsync(new RegisterJob());
         }
 
-        private void btnSearch(object sender, System.EventArgs e)
+        private async void btnSearch(object sender, System.EventArgs e)
         {
+            TxtResultsCount.Text = String.Empty;
+            Loading.IsVisible = true;
+            Loading.IsRunning = true;
+            NoResult.IsVisible = false;
 
+            string word = TxtWord.Text;
+            string cityState = TxtCityState.Text;
+
+            _searchParams = new SearchParams() { Word = word, CityState = cityState, NumberOfPage = 1 };
+
+            ResponseService<List<Job>> responseService = await _service.GetJobs(_searchParams.Word, _searchParams.CityState, _searchParams.NumberOfPage);
+
+            if (responseService.IsSuccess)
+            {
+                _listOfJobs = new ObservableCollection<Job>(responseService.Data);
+                _listOfJobsFirstRequest = _listOfJobs.Count();
+                ListOfJobs.ItemsSource = _listOfJobs;
+                ListOfJobs.RemainingItemsThreshold = 1;
+
+                TxtResultsCount.Text = $"{responseService.Pagination.TotalItems} resultado(s).";
+            }
+            else
+            {
+                await DisplayAlert("Erro!", "Oops! Ocorreu um erro inesperado! Tente novamente mais tarde.", "OK");
+            }
+            if (_listOfJobs.Count == 0)
+            {
+                NoResult.IsVisible = true;
+            }
+            else
+            {
+                NoResult.IsVisible = false;
+            }
+            Loading.IsVisible = false;
+            Loading.IsRunning = false;
         }
 
         private void btnVisualizerPage(object sender, System.EventArgs e)
         {
-            Navigation.PushAsync(new Visualizer());
+            var eventArgs = (TappedEventArgs)e;
+
+            var page = new Visualizer();
+            page.BindingContext = eventArgs.Parameter;
+
+            Navigation.PushAsync(page);
         }
 
         private void btnFocusPesquisa(object sender, System.EventArgs e)
         {
-            txtPesquisa.Focus();
+            TxtWord.Focus();
         }
 
         private void btnFocusCidadeEstado(object sender, System.EventArgs e)
         {
-            txtCidadeEstado.Focus();
+            TxtCityState.Focus();
         }
 
         private async void btnLogout(object sender, System.EventArgs e)
@@ -41,6 +92,36 @@ namespace AppJobSearch.Views
             App.Current.Properties.Remove("User");
             await App.Current.SavePropertiesAsync();
             App.Current.MainPage = new Login();
+        }
+
+        private async void InfinitySearch(object sender, System.EventArgs e)
+        {
+            ListOfJobs.RemainingItemsThreshold = -1;
+            _searchParams.NumberOfPage++;
+
+            ResponseService<List<Job>> responseService = await _service.GetJobs(_searchParams.Word, _searchParams.CityState, _searchParams.NumberOfPage);
+
+            if (responseService.IsSuccess)
+            {
+                var listOfJobsFromService = responseService.Data;
+                foreach (var item in listOfJobsFromService)
+                {
+                    _listOfJobs.Add(item);
+                }
+                if (_listOfJobsFirstRequest == listOfJobsFromService.Count)
+                {
+                    ListOfJobs.RemainingItemsThreshold = 1;
+                }
+                else
+                {
+                    ListOfJobs.RemainingItemsThreshold = -1;
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro!", "Oops! Ocorreu um erro inesperado! Tente novamente mais tarde.", "OK");
+                ListOfJobs.RemainingItemsThreshold = 0;
+            }
         }
     }
 }
--- AppJobSearch/AppJobSearch/Views/Visualizer.xaml.cs
+++ AppJobSearch/AppJobSearch/Views/Visualizer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppJobSearch.Models;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,6 +17,30 @@ namespace AppJobSearch.Views
         private void btnStartPage(object sender, EventArgs e)
         {
             Navigation.PopAsync();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Job job = ((Job)BindingContext);
+
+            if (string.IsNullOrEmpty(job.CompanyDescription))
+            {
+                HeaderCompanyDescription.IsVisible = false;
+                TextCompanyDescription.IsVisible = false;
+            }
+
+            if (string.IsNullOrEmpty(job.JobDescription))
+            {
+                HeaderJobDescription.IsVisible = false;
+                TextJobDescription.IsVisible = false;
+            }
+
+            if (string.IsNullOrEmpty(job.Benefits))
+            {
+                HeaderBenefits.IsVisible = false;
+                TextBenefits.IsVisible = false;
+            }
         }
     }
 }
